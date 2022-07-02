a7e6a8702f21d6c071fb4050256bb6c91052042a
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-07-02 23:19:00 +0530

    Added Employee payroll service

---
+++ src/com/bridgelabz/IO/EmployeePayrolData.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.IO;
+
+public class EmployeePayrolData {
+    int id;
+    String name;
+    double salary;
+
+    EmployeePayrolData() {
+    }
+
+    public EmployeePayrolData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
